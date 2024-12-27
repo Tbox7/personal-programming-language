b5a97a4d8c0916f17be18d5679b557fc379c18d2
Tb0x <60563088+Tbox7@users.noreply.github.com>
2024-12-27 13:12:12 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+def remainging_fuel(initial, kilometers, ratio):
+    return initial - kilometers * ratio
+initial = 50
+kilometers = 10
+ratio = 5
+print(remainging_fuel(initial, kilometers, ratio))
